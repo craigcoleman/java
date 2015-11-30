d06ac7c738c06461fc794e3b8cfa22b36002c502
craigcoleman <coleman@owt.com>
2015-11-30 05:05:44 -0800

    add all

---
+++ FileWriterEx.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+class FileWriterEx {
+    public static void main(String [] args) {
+        FileWriter fw= null;
+        File file =null;
+        try {
+            file=new File("WriteFile.txt");
+            if(!file.exists()) {
+                file.createNewFile();
+            }
+            fw = new FileWriter(file);
+            fw.write("This is an string written to a file");
+            fw.flush();
+            fw.close();
+            System.out.println("File written Succesfully");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
