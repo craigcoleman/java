2bfc9e4b867ceb6b918dd4f528d77922e2487f08
Craig Coleman <craigcoleman@users.noreply.github.com>
2016-09-29 10:11:20 -0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,14 @@
+// save file as Hello.java in a folder called java
+// compile wih 		$javac Hello.java
+// run with   		$java Hello
+
+class Hello {
+	public static void main (String args[]){
+		int i;
+		for (i = 0; i < 10; i ++){
+			System.out.print(i+" hello world ");
+		}
+		System.out.println();
+		
+	}
+}
---
+++ Text1.java
@@ -0,0 +1,22 @@
+// save this file as Text1.java
+// javac Text1.java
+class Text1 {
+public static void main (String a[]){
+		int i, ci;  // integer i, ci
+		char c;
+		i = 7;
+		c = 'A';
+		String s = "throw me a bone";//define String s
+		System.out.println("*******************");
+		System.out.println(i);
+		System.out.println(c);
+		System.out.println(s);
+		System.out.println("*******************");
+		i = i % 7;
+		ci = (int)c;	
+		System.out.println(i);
+		System.out.println(c);
+		System.out.println(ci);
+		
+	}
+}
