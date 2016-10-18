2979adbe33bf5a52f2d8b9006aeaebbd38c53118
craigcoleman <coleman@owt.com>
2016-10-18 13:44:12 -0700

    added all

---
+++ code2/Overflow.java
@@ -0,0 +1,14 @@
+class OverFlow {
+
+	public static void main (String a[]){
+		// 2^31 = 2147483648
+		int s = 2147483000;
+				for (int i = 0; i < 1000;i++){
+					s = s + 1;
+					System.out.print(" "+s);
+				}
+			
+
+	}
+}
+
